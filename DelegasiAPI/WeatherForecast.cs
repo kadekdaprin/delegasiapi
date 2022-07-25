a87fa5c50022e9ee17d9d5e0f4bd721f6015299e
Kadek Daprin Wakaliana <daprinwa@gmail.com>
2022-07-25 15:58:32 +0800

    tambah data forecast

--- DelegasiAPI/WeatherForecast.cs
+++ DelegasiAPI/WeatherForecast.cs
@@ -1,7 +1,85 @@
 namespace DelegasiAPI
 {
+    public static class WeatherForecastData
+    {
+        public static List<WeatherForecast> weatherForecasts = new List<WeatherForecast>()
+        {
+            new WeatherForecast()
+            {
+                Id = 1,
+                Date = DateTime.Now,
+                TemperatureC = Random.Shared.Next(-11, 32),
+                Summary = "Freezing",
+            },
+            new WeatherForecast()
+            {
+                Id = 2,
+                Date = DateTime.Now,
+                TemperatureC = Random.Shared.Next(-11, 32),
+                Summary = "Bracing",
+            },
+            new WeatherForecast()
+            {
+                Id = 3,
+                Date = DateTime.Now,
+                TemperatureC = Random.Shared.Next(-11, 32),
+                Summary = "Chilly",
+            },
+            new WeatherForecast()
+            {
+                Id = 4,
+                Date = DateTime.Now,
+                TemperatureC = Random.Shared.Next(-11, 32),
+                Summary = "Cool",
+            },
+            new WeatherForecast()
+            {
+                Id = 5,
+                Date = DateTime.Now,
+                TemperatureC = Random.Shared.Next(-11, 32),
+                Summary = "Mild",
+            },
+            new WeatherForecast()
+            {
+                Id = 6,
+                Date = DateTime.Now,
+                TemperatureC = Random.Shared.Next(-11, 32),
+                Summary = "Warm",
+            },
+            new WeatherForecast()
+            {
+                Id = 7,
+                Date = DateTime.Now,
+                TemperatureC = Random.Shared.Next(-11, 32),
+                Summary = "Balmy",
+            },
+            new WeatherForecast()
+            {
+                Id = 8,
+                Date = DateTime.Now,
+                TemperatureC = Random.Shared.Next(-11, 32),
+                Summary = "Hot",
+            },
+            new WeatherForecast()
+            {
+                Id = 9,
+                Date = DateTime.Now,
+                TemperatureC = Random.Shared.Next(-11, 32),
+                Summary = "Sweltering",
+            },
+            new WeatherForecast()
+            {
+                Id = 10,
+                Date = DateTime.Now,
+                TemperatureC = Random.Shared.Next(-11, 32),
+                Summary = "Scorching"
+            },
+        };
+    }
+
     public class WeatherForecast
     {
+        public int Id { get; set; }
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
