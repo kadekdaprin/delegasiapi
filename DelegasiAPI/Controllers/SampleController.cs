f762f626a5e70cab02ad9692b6a34543014112cf
Kadek Daprin Wakaliana <daprinwa@gmail.com>
2022-08-01 13:29:34 +0800

    Tambah Sample get by id

--- DelegasiAPI/Controllers/SampleController.cs
+++ DelegasiAPI/Controllers/SampleController.cs
@@ -20,25 +20,37 @@ namespace DelegasiAPI.Controllers
         [HttpGet]
         public ActionResult<List<SampleModel>> Get(string? nama = null)
         {
-            using (var conn = new SqlConnection(_connectionString))
+            using var conn = new SqlConnection(_connectionString);
+            List<SampleModel> result;
+
+            var query = "SELECT * FROM SampleTable";
+
+            Dictionary<string, object> param = new();
+
+            if (!string.IsNullOrEmpty(nama))
             {
-                List<SampleModel> result;
+                query += " WHERE Name LIKE @nama";
 
-                var query = "SELECT * FROM SampleTable";
-
-                Dictionary<string, object> param = new();
-
-                if (!string.IsNullOrEmpty(nama))
-                {
-                    query += " WHERE Name LIKE @nama";
-
-                    param.Add("nama",$"%{nama}%" );
-                }
-                
-                result = conn.Query<SampleModel>(query, param).ToList();
-
-                return Ok(result);
+                param.Add("nama", $"%{nama}%");
             }
+
+            result = conn.Query<SampleModel>(query, param).ToList();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<SampleModel> Get(int? id = null)
+        {
+            using var conn = new SqlConnection(_connectionString);
+
+            var query = "SELECT TOP 1 * FROM SampleTable WHERE Id = @id";
+
+            var result = conn.QueryFirstOrDefault<SampleModel>(query, new { id });
+
+            if (result == null) return NotFound();
+
+            return Ok(result);
         }
     }
 }
